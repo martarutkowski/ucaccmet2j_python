9cd726e77890c5399623d049830f61240859e2fb
martarutkowski <rutkowski.marta2004@gmail.com>
2025-01-14 14:26:25 +0100

    created a dictionary with total monthly precipitation

--- precipitation_assignment.py
+++ precipitation_assignment.py
@@ -1,4 +1,5 @@
 import json
+import csv
 
 # open precipitation file
 with open ('precipitation.json') as file:
@@ -11,7 +12,17 @@ seattle_station = 'GHCND:USW00093814'
 seattle_data = []
 for measurements in contents:
     if (measurements['station']) == seattle_station:
-        seattle_data.append(measurements['value'])
+        seattle_data.append(measurements)
 
+# calculate total precipitation by month
+total_monthly_precipitation = {}
 
+#for values in seattle_data:
+for measurements in seattle_data:
+    month = int(measurements['date'].split('-')[1]) #spliting the date, taking the months and make it an integer
+    # add precipitation to the corresponding month
+    if month in total_monthly_precipitation:
+        total_monthly_precipitation[month] += measurements['value'] # add precipitation if month in the dictionary
+    else:
+        total_monthly_precipitation[month] = 0  # enter the month if not already in the dictionary
 
