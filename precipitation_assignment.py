a35a8c1c255ff53dab5ca3b8c005ef6cc5ca1641
martarutkowski <rutkowski.marta2004@gmail.com>
2025-01-14 14:46:07 +0100

    made a json file with the data

--- precipitation_assignment.py
+++ precipitation_assignment.py
@@ -5,13 +5,15 @@ import csv
 with open ('precipitation.json') as file:
     contents = json.load(file)
 
-# station code of Seatlle
-seattle_station = 'GHCND:USW00093814'
+# Seatlle data
+station_number = 'GHCND:USW00093814'
+state = 'WA'
+city_name = 'Seattle'
 
 # create a list with the values of precipitation
 seattle_data = []
 for measurements in contents:
-    if (measurements['station']) == seattle_station:
+    if (measurements['station']) == station_number:
         seattle_data.append(measurements)
 
 # calculate total precipitation by month
@@ -26,3 +28,15 @@ for measurements in seattle_data:
     else:
         total_monthly_precipitation[month] = 0  # enter the month if not already in the dictionary
 
+# difine a dictionary to store information
+precipitation_info = {}
+
+#store the results in the precipitation information dictionary
+precipitation_info[city_name] = {
+        'station' : station_number,
+        'state' : state,
+        'total_monthly_precipitation' : total_monthly_precipitation
+}
+#store the results in results.json file
+with open('results.json','w') as file:
+    json.dump(precipitation_info,file, indent=4)
